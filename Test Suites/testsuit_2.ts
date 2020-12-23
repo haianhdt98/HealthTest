87b37e4080466dd6fe19436f9c0d4e59c00bab3b
anhphamdt98 <pham.hai.anh@sun-asterisk.com>
2020-12-23 16:54:30 +0700

    mazii_continue

---
+++ Test Suites/testsuit_2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuit_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5ac03eda-18c5-4721-ae9b-07d961c7dfd7</testSuiteGuid>
+   <testCaseLink>
+      <guid>3954815b-30f7-4346-b399-ab7a9cd5ac47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mazii_01</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
