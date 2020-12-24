110a8d82ea5a0189d74c493df82deb9e19484979
anhphamdt98 <pham.hai.anh@sun-asterisk.com>
2020-12-24 15:38:14 +0700

    create testsuit_dict

---
+++ Test Suites/testsuit_dict.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuit_dict</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9db0b6d3-2475-4213-91f4-0dc3ae973902</testSuiteGuid>
+</TestSuiteEntity>
