1406915a5321b19ef1c21f4c8ba0101ffc3d1944
anhphamdt98 <pham.hai.anh@sun-asterisk.com>
2020-12-24 15:49:06 +0700

    testsuit_dict_02

--- Test Suites/testsuit_dict_02.ts
+++ Test Suites/testsuit_dict_02.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>testsuit_dict</name>
+   <name>testsuit_dict_02</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,5 +10,11 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>9db0b6d3-2475-4213-91f4-0dc3ae973902</testSuiteGuid>
+   <testSuiteGuid>000201cf-a3cd-4a77-98b7-1c545ab33849</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb63d4b2-76ce-4119-9b3d-40299fe07bb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/dict_Vi_En</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
